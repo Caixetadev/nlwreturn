831b98eca4521127fed2c527455355964ec10430
Rafael Caixeta <caixetadev@gmail.com>
2022-05-05 15:12:20 -0300

    feat: created functionality of taking screenshot and separating in a component

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,5 @@
 import { ArrowLeft } from 'phosphor-react'
+import { useState } from 'react'
 import { FeedbackType, feedbackTypes } from '..'
 import { CloseButton } from '../../CloseButton'
 import { ScreenshotButton } from '../ScreenshotButton'
@@ -12,6 +13,8 @@ export function FeedbackContentStep({
   onFeedbackRestartRequested,
   feedbackType
 }: FeedbackContetStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null)
+
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
   return (
@@ -43,7 +46,10 @@ export function FeedbackContentStep({
         />
 
         <footer className="flex gap-2 mt-2">
-          <ScreenshotButton />
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
 
           <button
             type="submit"
